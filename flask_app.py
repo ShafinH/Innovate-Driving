65beeb00f347929d822b823588e4797c289be415
ShafinH <shafin1025@gmail.com>
2020-10-03 13:05:07 -0700

    added distracted

--- flask_app.py
+++ flask_app.py
@@ -14,12 +14,12 @@ def home():
 
 
 @app.route('/pred/')
-def rec():
+def sever():
     return render_template('severity.html')
 
 
-@app.route('/recommend', methods=['POST'])
-def recommend():
+@app.route('/predict', methods=['POST'])
+def predict():
 
     data = request.form['sev']
 
@@ -29,6 +29,26 @@ def recommend():
     return render_template('index.html', prediction_text='. {}'.format(output[0]), prediction_text2='. {}'.format(output[1]),
                            prediction_text3='. {}'.format(output[2]), prediction_text4='. {}'.format(output[3]), prediction_text5='. {}'.format(output[4]),
                            message='Your accident has been predicited to be a stage', pred=output, one=1, two=2, three=3, four=4, five=5)
+
+
+# distracted driver
+
+@app.route('/distracted/')
+def distr():
+    return render_template('distracted.html')
+
+
+@app.route('/classify', methods=['POST'])
+def classify():
+
+    data = request.form['distracted']
+
+    output = model.predict(data)
+    # call function
+
+    return render_template('index.html', prediction_text='. {}'.format(output[0]), prediction_text2='. {}'.format(output[1]),
+                           prediction_text3='. {}'.format(output[2]), prediction_text4='. {}'.format(output[3]), prediction_text5='. {}'.format(output[4]),
+                           message='The driver is ', pred=output, one=1, two=2, three=3, four=4, five=5)
 
 
 if __name__ == "__main__":
